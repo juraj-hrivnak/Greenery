26af5ad026dc19aec3635a06e30a5cda7bb3d909
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-07-18 20:07:38 +0200

    Fix bonemeal placing ghost blocks

--- src/main/java/teksturepako/greenery/common/event/EventBonemeal.kt
+++ src/main/java/teksturepako/greenery/common/event/EventBonemeal.kt
@@ -8,11 +8,11 @@ import net.minecraft.init.Blocks
 import net.minecraft.util.EnumParticleTypes
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
+import net.minecraftforge.common.util.Constants
 import net.minecraftforge.event.entity.player.BonemealEvent
 import net.minecraftforge.fml.common.Loader
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.Event
-import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import teksturepako.greenery.common.block.plant.aquatic.AbstractAquaticPlant
 import teksturepako.greenery.common.registry.ModBlocks
@@ -20,123 +20,89 @@ import java.util.*
 
 @Mod.EventBusSubscriber
 object EventBonemeal {
-    private const val SEAGRASS_GENERATION_ATTEMPTS = 8
+    private const val GENERATION_ATTEMPTS = 8
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    @JvmStatic fun onBonemealUsedOnGrass(event: BonemealEvent) {
-        if (event.block.material == Material.GRASS && event.block.isFullBlock) {
-            grow(event.world, event.world.rand, event.pos)
-            event.isCanceled = true
-        }
-    }
+    private fun spawnParticles(worldIn: World, pos: BlockPos, rand: Random) {
+        for (i in 0 until GENERATION_ATTEMPTS) {
+            val blockPos = pos.add(
+                rand.nextInt(4) - rand.nextInt(4),
+                rand.nextInt(2) - rand.nextInt(2),
+                rand.nextInt(4) - rand.nextInt(4)
+            )
 
-    private fun grow(worldIn: World, rand: Random, pos: BlockPos) {
-        val defaultPos = pos.up()
-        for (i in 0..127) {
-            var blockPos = defaultPos
-            for (j in 0 until i / 8) {
-                blockPos = blockPos.add(
-                    rand.nextInt(3) - 1,
-                    (rand.nextInt(3) - 1) * rand.nextInt(3) / 2,
-                    rand.nextInt(3) - 1
-                )
-                if (worldIn.getBlockState(blockPos.down()).block !== Blocks.GRASS ||
-                    worldIn.getBlockState(blockPos).isNormalCube
-                ) continue
+            var d1 = (blockPos.x.toFloat() + rand.nextFloat()).toDouble()
+            var d2 = (blockPos.y.toFloat() + rand.nextFloat()).toDouble()
+            var d3 = (blockPos.z.toFloat() + rand.nextFloat()).toDouble()
+            when {
+                i == 0 && !worldIn.getBlockState(blockPos.up()).isOpaqueCube     -> d2 = blockPos.y.toDouble() + 0.0625 + 1.0
+                i == 1 && !worldIn.getBlockState(blockPos.down()).isOpaqueCube   -> d2 = blockPos.y.toDouble() - 0.0625
+                i == 2 && !worldIn.getBlockState(blockPos.south()).isOpaqueCube  -> d3 = blockPos.z.toDouble() + 0.0625 + 1.0
+                i == 3 && !worldIn.getBlockState(blockPos.north()).isOpaqueCube  -> d3 = blockPos.z.toDouble() - 0.0625
+                i == 4 && !worldIn.getBlockState(blockPos.east()).isOpaqueCube   -> d1 = blockPos.x.toDouble() + 0.0625 + 1.0
+                i == 5 && !worldIn.getBlockState(blockPos.west()).isOpaqueCube   -> d1 = blockPos.x.toDouble() - 0.0625
             }
-            if (worldIn.isAirBlock(blockPos)) {
-                if (rand.nextInt(8) == 0) {
-                    worldIn.getBiome(blockPos).plantFlower(worldIn, rand, blockPos)
-                    spawnParticles(worldIn, blockPos)
-                } else {
-                    val block = ModBlocks.blockTallGrass
-                    val startingAge = rand.nextInt(block.maxAge)
-                    val state = block.defaultState.withProperty(block.ageProperty, startingAge)
-
-                    if (block.canBlockStay(worldIn, blockPos, state)) {
-                        worldIn.setBlockState(blockPos, state, 3)
-                        spawnParticles(worldIn, blockPos)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun spawnParticles(worldIn: World, pos: BlockPos) {
-        if (!worldIn.isAirBlock(pos)) {
-            val random = worldIn.rand
-            for (i in 0..5) {
-                var d1 = (pos.x.toFloat() + random.nextFloat()).toDouble()
-                var d2 = (pos.y.toFloat() + random.nextFloat()).toDouble()
-                var d3 = (pos.z.toFloat() + random.nextFloat()).toDouble()
-                if (i == 0 && !worldIn.getBlockState(pos.up()).isOpaqueCube) {
-                    d2 = pos.y.toDouble() + 0.0625 + 1.0
-                }
-                if (i == 1 && !worldIn.getBlockState(pos.down()).isOpaqueCube) {
-                    d2 = pos.y.toDouble() - 0.0625
-                }
-                if (i == 2 && !worldIn.getBlockState(pos.south()).isOpaqueCube) {
-                    d3 = pos.z.toDouble() + 0.0625 + 1.0
-                }
-                if (i == 3 && !worldIn.getBlockState(pos.north()).isOpaqueCube) {
-                    d3 = pos.z.toDouble() - 0.0625
-                }
-                if (i == 4 && !worldIn.getBlockState(pos.east()).isOpaqueCube) {
-                    d1 = pos.x.toDouble() + 0.0625 + 1.0
-                }
-                if (i == 5 && !worldIn.getBlockState(pos.west()).isOpaqueCube) {
-                    d1 = pos.x.toDouble() - 0.0625
-                }
-                if (d1 < pos.x.toDouble() || d1 > (pos.x + 1).toDouble() || d2 < 0.0 || d2 > (pos.y + 1).toDouble() || d3 < pos.z.toDouble() || d3 > (pos.z + 1).toDouble()) {
-                    worldIn.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, d1, d2, d3, 0.0, 0.0, 0.0)
-                }
+            if (d1 < blockPos.x.toDouble() || d1 > (blockPos.x + 1).toDouble() || d2 < 0.0 || d2 > (blockPos.y + 1).toDouble() || d3 < blockPos.z.toDouble() || d3 > (blockPos.z + 1).toDouble()) {
+                worldIn.spawnParticle(EnumParticleTypes.VILLAGER_HAPPY, d1, d2, d3, 0.0, 0.0, 0.0)
             }
         }
     }
 
     @SubscribeEvent
-    @JvmStatic fun onBonemealUsedUnderwater(event: BonemealEvent) {
+    @JvmStatic fun onBonemealUsed(event: BonemealEvent) {
         val world = event.world
         val block = event.block
         val up = event.pos.up()
         val upBlock = world.getBlockState(up).block
+        val rand = world.rand
+
+        if (event.block.material == Material.GRASS && event.block.isFullBlock) {
+            if (!world.isRemote) {
+                growGrass(up, world, rand)
+                event.result = Event.Result.ALLOW
+            } else if (event.entityPlayer == Minecraft.getMinecraft().player) {
+                Minecraft.getMinecraft().player.swingArm(event.hand!!)
+                spawnParticles(world, up, rand)
+            }
+            event.isCanceled = true
+        }
 
         if (Loader.isModLoaded("simpledifficulty")) {
             if ((upBlock == Blocks.WATER || upBlock == blockPurifiedWater) &&
                 block.material in AbstractAquaticPlant.ALLOWED_SOILS
             ) {
                 if (!world.isRemote) {
-                    growRivergrass(up, world)
+                    growRivergrass(up, world, rand)
                     event.result = Event.Result.ALLOW
                 } else if (event.entityPlayer == Minecraft.getMinecraft().player) {
                     Minecraft.getMinecraft().player.swingArm(event.hand!!)
+                    spawnParticles(world, up, rand)
                 }
             }
 
             if (upBlock == blockSaltWater && block.material in AbstractAquaticPlant.ALLOWED_SOILS) {
                 if (!world.isRemote) {
-                    growSeagrass(up, world)
+                    growSeagrass(up, world, rand)
                     event.result = Event.Result.ALLOW
                 } else if (event.entityPlayer == Minecraft.getMinecraft().player) {
                     Minecraft.getMinecraft().player.swingArm(event.hand!!)
+                    spawnParticles(world, up, rand)
                 }
             }
         } else {
             if (upBlock == Blocks.WATER && block.material in AbstractAquaticPlant.ALLOWED_SOILS) {
                 if (!world.isRemote) {
-                    growSeagrass(up, world)
+                    growSeagrass(up, world, rand)
                     event.result = Event.Result.ALLOW
                 } else if (event.entityPlayer == Minecraft.getMinecraft().player) {
                     Minecraft.getMinecraft().player.swingArm(event.hand!!)
+                    spawnParticles(world, up, rand)
                 }
             }
         }
     }
 
-    private fun growSeagrass(pos: BlockPos, world: World) {
-        val rand = world.rand
-
-        for (i in 0 until SEAGRASS_GENERATION_ATTEMPTS) {
+    private fun growSeagrass(pos: BlockPos, world: World, rand: Random) {
+        for (i in 0 until GENERATION_ATTEMPTS) {
             val blockPos = pos.add(
                 rand.nextInt(4) - rand.nextInt(4),
                 rand.nextInt(2) - rand.nextInt(2),
@@ -159,10 +125,8 @@ object EventBonemeal {
         }
     }
 
-    private fun growRivergrass(pos: BlockPos, world: World) {
-        val rand = world.rand
-
-        for (i in 0 until SEAGRASS_GENERATION_ATTEMPTS) {
+    private fun growRivergrass(pos: BlockPos, world: World, rand: Random) {
+        for (i in 0 until GENERATION_ATTEMPTS) {
             val blockPos = pos.add(
                 rand.nextInt(4) - rand.nextInt(4),
                 rand.nextInt(2) - rand.nextInt(2),
@@ -180,6 +144,21 @@ object EventBonemeal {
                 if (block == Blocks.WATER && ModBlocks.blockRivergrass.canPlaceBlockAt(world, blockPos)) {
                     world.setBlockState(blockPos, ModBlocks.blockRivergrass.defaultState)
                 }
+            }
+        }
+    }
+
+    private fun growGrass(pos: BlockPos, world: World, rand: Random) {
+        for (i in 0 until GENERATION_ATTEMPTS) {
+            val blockPos = pos.add(
+                rand.nextInt(4) - rand.nextInt(4),
+                rand.nextInt(2) - rand.nextInt(2),
+                rand.nextInt(4) - rand.nextInt(4)
+            )
+
+            if (ModBlocks.blockTallGrass.canPlaceBlockAt(world, blockPos)) {
+                world.notifyBlockUpdate(blockPos, world.getBlockState(blockPos), ModBlocks.blockTallGrass.defaultState, Constants.BlockFlags.DEFAULT_AND_RERENDER)
+                world.setBlockState(blockPos, ModBlocks.blockTallGrass.defaultState)
             }
         }
     }
