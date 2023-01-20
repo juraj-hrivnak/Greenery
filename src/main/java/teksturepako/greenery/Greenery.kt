a35a6b1a988f2cea72a9128fd45c3f2de85294b5
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-01-20 19:39:52 +0100

    Bump version

--- src/main/java/teksturepako/greenery/Greenery.kt
+++ src/main/java/teksturepako/greenery/Greenery.kt
@@ -52,7 +52,7 @@ object Greenery
 {
     const val MODID = "greenery"
     const val NAME = "Greenery"
-    const val VERSION = "2.7"
+    const val VERSION = "2.8"
     const val DEPENDENCIES = "required-after:forgelin@[1.8.4,);required-after:fluidlogged_api@[1.9.0.5,);after:dynamictrees;" +
                              "after:biomesoplenty"
     const val ACCEPTED_MINECRAFT_VERSIONS = "[1.12,1.12.2,)"
